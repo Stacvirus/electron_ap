3f9caf61b73d788ea0a5dad80bc475cbdecfe56c
takam andre <andre@pluriel.tech>
2025-02-14 20:23:54 +0100

    init: hello world script

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("hello world!")
+const {app, BrowserWindow} = require("electron");
+const url = require("url");
+const path = require("path");
+
+function createMainWindow(){
+  const mainWindow = new BrowserWindow({
+    title: "Hello world!",
+    width: 1000,
+    height: 600,
+  })
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, "index.html"),
+    protocol: "file",
+  });
+
+  mainWindow.loadURL(startUrl);
+}
+
+app.whenReady().then(createMainWindow)
